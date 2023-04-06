class/sample weighting fix

learners/default.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 import models
 
 import copy
-from collections import Counter
+from collections import Counter, OrderedDict
 
 import numpy as np
 import os, math
@@ -116,10 +116,9 @@ class NormalNN(nn.Module):
         if self.epochs_of_interest[-1]!=self.config['schedule'][-1]:
             self.epochs_of_interest.append(self.config['schedule'][-1])
         
-        # self.steps_of_interest = []
-        self.steps_of_interest = [i for i in range(0, self.steps, int(self.steps/10))] if self.steps!=-1 else []
-        # if self.steps_of_interest[1]!=1:
-        #     self.steps_of_interest.insert(1,1)
+        self.steps_of_interest = [i for i in range(0, self.steps, int(self.steps/100))] if self.steps!=-1 else []
+        if self.steps!=-1 and self.steps_of_interest[1]!=1:
+            self.steps_of_interest.insert(1,1)
         if self.steps!=-1 and self.steps_of_interest[-1]!=self.steps:
             self.steps_of_interest.append(self.steps)
        
@@ -154,6 +153,7 @@ class NormalNN(nn.Module):
         if not self.is_oracle and self.replay and self.is_custom_replay_loader:
             self.replay_loader_iter = cycle(self.replay_loader)
 
+        self.data_weights = OrderedDict()
 
     def init_params_epoch(self):
 
@@ -162,9 +162,7 @@ class NormalNN(nn.Module):
         self.per_class_old_features = {}
         self.per_class_new_features = {}
         
-        if not self.is_oracle:
-            self.data_weights = torch.Tensor([]).cuda()
-            self.data_indices = torch.Tensor([])
+        
             # if self.replay:
             #     self.old_classes = set([self.replay_dataset.class_mapping[int(y)] for y in self.replay_dataset.targets])
                 # if self.is_custom_replay_loader:
@@ -310,7 +308,7 @@ class NormalNN(nn.Module):
 
         return loss, output
         
-    def scale_distribution(self, data, classes, rnge=(1,7)):
+    def scale_distribution(self, data, classes, rnge=(1,8)):
 
         t_max = rnge[0]
         t_min = rnge[1]
@@ -350,11 +348,11 @@ class NormalNN(nn.Module):
                 elif int(self.class_weighting_with)==2: ## custom weighting 50%-50%
                     self.class_replay_ratios= { cl: self.class_ratios_manual[cl] for cl in self.class_ratios_manual if cl in self.new_classes}
                 elif int(self.class_weighting_with)==11 and self.epoch!=0: ## dist shift
-                    classes = [ list(self.class_dist_shift_epoch[k].keys()) for k in self.class_dist_shift_epoch][0]
-                    class_dist_shift_epoch_mean = np.mean([ list(self.class_dist_shift_epoch[k].values()) for k in self.class_dist_shift_epoch], axis=0)
-                    self.scale_distribution(class_dist_shift_epoch_mean, classes)
+                    classes = list(self.class_dist_shift_epoch.keys())
+                    class_dist_shift_epoch = np.array(list(self.class_dist_shift_epoch.values()))
+                    self.scale_distribution(class_dist_shift_epoch, classes)
                 elif int(self.class_weighting_with)==12: ## acc shift
-                    classes = [ list(self.class_accuracy_epoch.keys()) ][0]
+                    classes = list(self.class_accuracy_epoch.keys()) 
                     class_accuracy_epoch = np.array(list(self.class_accuracy_epoch.values()))
                     # print (self.class_accuracy_epoch)
                     self.scale_distribution(class_accuracy_epoch, classes)
@@ -362,9 +360,9 @@ class NormalNN(nn.Module):
                 elif int(self.class_weighting_with)==13: ## sim shift TODO not working, incorptorate in dynamic replay criterion/loss function
                     if self.replay:
                         base_path = self.plot_dir+'_after/new_vs_old/epoch_' + str(self.epoch)+'/' if self.epoch>0 else self.plot_dir+'_before/new_vs_old/'
-                        self.sim_table, self.sim_to_new_cls = new_vs_old_class_comparison(self.new_classes, self.old_classes, self.per_class_new_features, self.labels_to_names, self.class_mapping, replay_size = self.replay_size, base_path=base_path, is_oracle=self.is_oracle)
+                        self.sim_table, self.sim_to_new_cls = new_vs_old_class_comparison(self.new_classes, self.old_classes, self.processed_per_class_new_features, self.labels_to_names, self.class_mapping, replay_size = self.replay_size, base_path=base_path, is_oracle=self.is_oracle)
                         # self.sim_to_new_cls = 1-np.array([[ sim_table[k][cl] for cl in sim_table[k]] for k in sim_table]).mean(0) ## for interference
-                        classes = [ list(self.sim_table[k].keys()) for k in self.sim_table][0]
+                        classes = list(self.sim_table.keys())
                         self.scale_distribution(self.sim_to_new_cls, classes)
                     else:
                         # self.class_ratios = { k: 1 for k in self.classes}

learners/er.py
@@ -84,26 +84,29 @@ class TR(NormalNN):
     def batch_data_weighting(self, output, labels, indices, train_data_batch_size):
         
         if not self.replay_strategy:
-            self.data_weights = torch.concat([self.data_weights, torch.ones(len(indices[:train_data_batch_size])).cuda() ]) 
-            self.data_indices = torch.concat([self.data_indices, indices[:train_data_batch_size]]) 
+            # self.data_weights = torch.concat([self.data_weights, torch.ones(len(indices[:train_data_batch_size])).cuda() ]) 
+            # self.data_indices = torch.concat([self.data_indices, indices[:train_data_batch_size]]) 
+            for ix in indices[:train_data_batch_size]:
+                self.data_weights[int(ix)] = 1
             return
 
-        if self.epoch==self.epochs_of_interest[-1]:
-            batch_weights = dataloaders.compute_weights(self.replay_strategy, output[:train_data_batch_size], labels[:train_data_batch_size])
-            # if self.is_custom_replay_loader and hasattr(self, 'replay_loader') and self.is_batch_sampler:
-            #     self.replay_loader.batch_sampler.update_weights(batch_new_weights=batch_weights, indices=len(self.replay_loader.batch_sampler)+indices[:train_data_batch_size])
-            
-            self.data_weights = torch.concat([self.data_weights, batch_weights]) 
-            self.data_indices = torch.concat([self.data_indices, indices[:train_data_batch_size]]) 
+        
+        batch_weights = dataloaders.compute_weights(self.replay_strategy, output[:train_data_batch_size], labels[:train_data_batch_size])
+        # if self.is_custom_replay_loader and hasattr(self, 'replay_loader') and self.is_batch_sampler:
+        #     self.replay_loader.batch_sampler.update_weights(batch_new_weights=batch_weights, indices=len(self.replay_loader.batch_sampler)+indices[:train_data_batch_size])
+        for ix, w in zip(indices[:train_data_batch_size],batch_weights):
+            self.data_weights[int(ix)] = w
+        # self.data_weights = torch.concat([self.data_weights, batch_weights]) 
+        # self.data_indices = torch.concat([self.data_indices, indices[:train_data_batch_size]]) 
                 
         if self.replay:
             # replay_batch_weights = dataloaders.compute_weights(self.replay_strategy, output[train_data_batch_size:], labels[train_data_batch_size:])
             # if self.is_custom_replay_loader and self.is_batch_sampler:
             #     self.replay_loader.batch_sampler.update_weights(batch_new_weights=replay_batch_weights, indices=indices[train_data_batch_size:])
-            if self.epoch==self.epochs_of_interest[-1]: ## and self.is_custom_replay_loader?
+            # if (self.steps==-1 and self.epoch==self.epochs_of_interest[-1]) or (self.steps!=-1 and self.step_count==self.steps_of_interest[-1]): ## and self.is_custom_replay_loader?
                 # self.replay_loader.sampler.update_weights(batch_new_weights=replay_batch_weights, indices=indices[train_data_batch_size:])
-                replay_batch_weights = dataloaders.compute_weights(self.replay_strategy, output[train_data_batch_size:], labels[train_data_batch_size:]) ##TODO remove this and uncomment at line 100
-                self.replay_dataset.update_weights(batch_new_weights=replay_batch_weights, indices=indices[train_data_batch_size:])
+            replay_batch_weights = dataloaders.compute_weights(self.replay_strategy, output[train_data_batch_size:], labels[train_data_batch_size:]) ##TODO remove this and uncomment at line 100
+            self.replay_dataset.update_weights(batch_new_weights=replay_batch_weights, indices=indices[train_data_batch_size:])
         
     def learn_batch(self, train_loader, train_dataset, replay_dataset, model_save_dir, val_target, task, val_loader=None):
         
@@ -289,13 +292,16 @@ class TR(NormalNN):
             self.replay_dataset.extend(self.train_dataset, class_replay_ratios= self.class_replay_ratios)
         else:
             print ('class_replay_weights: ', { self.labels_to_names[self.class_mapping[cl]]: self.class_replay_weights[cl] for cl in self.class_replay_weights})
-            self.data_indices = torch.argsort(self.data_indices).long()
-            self.replay_dataset.extend(self.train_dataset.data[self.data_indices], self.train_dataset.targets[self.data_indices], self.train_dataset.class_mapping, class_replay_weights= self.class_replay_weights, weights = self.data_weights[self.data_indices], replay_strategy=self.replay_strategy)
+            # self.data_indices = torch.argsort(self.data_indices).long()
+            self.data_indices = torch.Tensor(list(self.data_weights.keys())).long()
+            self.data_weights = torch.Tensor(list(self.data_weights.values())).cuda()
+            
+            self.replay_dataset.extend(self.train_dataset.data[self.data_indices], self.train_dataset.targets[self.data_indices], self.train_dataset.class_mapping, class_replay_weights= self.class_replay_weights, weights = self.data_weights, replay_strategy=self.replay_strategy)
             
             self.num_replay_samples = self.num_replay_samples_init if self.num_replay_samples_init!=-1 else len(self.replay_dataset)
             if self.is_custom_replay_loader:
                 if not hasattr(self, 'replay_loader'):
-                    self.initialise_replay_loader(self.data_weights[self.data_indices])
+                    self.initialise_replay_loader(self.data_weights)
                 # if self.is_batch_sampler:
                 #     self.replay_loader.batch_sampler.sampler.num_samples=self.num_replay_samples
                 #     self.replay_loader.batch_sampler.sampler.class_weights= torch.Tensor(self.replay_dataset.class_weights).cuda()
@@ -303,7 +309,7 @@ class TR(NormalNN):
                 # self.replay_loader.sampler.num_samples=self.num_replay_samples
                 self.replay_loader.sampler.class_weights= torch.Tensor(self.replay_dataset.class_weights).cuda()
                 self.replay_loader.sampler.weights= torch.Tensor(self.replay_dataset.sample_weights).cuda()
-
+                print ('weights after task: ', self.data_weights)
             # if self.replay_strategy:
             #     self.replay_dataset.get_weight_distribution(self.replay_strategy)  
         print ('size of replay dataset:',  len(self.replay_dataset))